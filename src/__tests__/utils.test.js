265d0a6b60a875ffff5978249c64f8bf59e983a9
g;itchtonny <sirmwangitosh@gmail.com>
2024-05-23 20:31:39 +0300

    lab complete

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,36 @@
 // Your tests here
+
+import {isPalindrome} from "../utils";
+
+describe("isPalindrome",()=>{
+    it("should return true when the word from forward and backwards is the same", ()=>{
+
+        const word = "racecar";
+        const result = isPalindrome(word)
+        expect(result).toBe(true);
+    })
+
+    it("when the word is not the same forward and backward return false",()=>{
+
+        const word = "car"
+        const result = isPalindrome(word);
+        expect(result).toBe(false)
+    })
+    it("return true for words that are a combination of uppercase and lowercase letters", () => {
+        const word = "RaCecar";
+    
+        const result = isPalindrome(word);
+    
+        expect(result).toBe(true);
+      });
+      it("throws an error if the input has any non-alphabetic characters", () => {
+        const word = "race car";
+    
+        expect(() => isPalindrome(word)).toThrow();
+      });
+      it("throws an error if the input is an empty string", () => {
+        const word = "";
+    
+        expect(() => isPalindrome(word)).toThrow();
+      });
+})
